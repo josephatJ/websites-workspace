72f7a4c213894e611e6f7e9c4e52be9a8d161ce9
Josephat Julius M <josephatjulius24@gmail.com>
2025-11-05 17:17:49 +0300

    Release: Change responsive options configs

--- apps/general-website/src/app/shared/components/home-welcome-container/home-welcome-container.ts
+++ apps/general-website/src/app/shared/components/home-welcome-container/home-welcome-container.ts
@@ -44,22 +44,7 @@ export class HomeWelcomeContainer implements OnInit {
     }
     this.responsiveOptions = [
       {
-        breakpoint: '1400px',
-        numVisible: 2,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '1199px',
-        numVisible: 3,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '767px',
-        numVisible: 2,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '575px',
+        breakpoint: '1200px',
         numVisible: 1,
         numScroll: 1,
       },
