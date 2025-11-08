264ade108032a11fe3639fcaef5d7a44ecd24330
Josephat Julius M <josephatjulius24@gmail.com>
2025-11-08 23:19:59 +0300

    Add featured events list on featured events scroll and improve about summary

--- apps/general-website/src/app/shared/components/featured-events/featured-events.ts
+++ apps/general-website/src/app/shared/components/featured-events/featured-events.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SharedGeneralServiceAndState } from '../../services/general-state.service';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'app-featured-events',
-  imports: [],
+  imports: [CarouselModule, ButtonModule, TagModule],
   templateUrl: './featured-events.html',
   styleUrl: './featured-events.css',
 })
-export class FeaturedEvents {}
+export class FeaturedEvents implements OnInit {
+  private generalStateService = inject(SharedGeneralServiceAndState);
+  events = this.generalStateService.events;
+  responsiveOptions: any[] | undefined;
+
+  ngOnInit(): void {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1200px',
+        numVisible: 1,
+        numScroll: 2,
+      },
+    ];
+    if (!this.events || this.events()?.length === 0) {
+      this.generalStateService
+        .loadData(`items/events?fields=*,photos.*`)
+        .subscribe({
+          next: (events) => {
+            this.generalStateService.updateEvents(events);
+          },
+        });
+    }
+  }
+}
