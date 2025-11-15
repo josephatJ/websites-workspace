2b284bd5ef15b29b264a36aafcf926c500a066e3
Josephat Julius M <josephatjulius24@gmail.com>
2025-11-15 20:47:46 +0300

    Release: Add google maps definitions

--- apps/general-website/src/app/shared/components/maps-location-container/maps-location-container.ts
+++ apps/general-website/src/app/shared/components/maps-location-container/maps-location-container.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-maps-location-container',
-  imports: [],
+  imports: [GoogleMapsModule],
   templateUrl: './maps-location-container.html',
   styleUrl: './maps-location-container.css',
 })
-export class MapsLocationContainer {}
+export class MapsLocationContainer {
+  @ViewChild(GoogleMap) map!: GoogleMap;
+  @ViewChild(MapMarker) marker!: MapMarker;
+
+  zoom = 14;
+  center: google.maps.LatLngLiteral = { lat: -6.7687728, lng: 39.2278758 };
+  markerPosition: google.maps.LatLngLiteral = this.center;
+
+  locationText = `Lat: ${this.center.lat}, Lng: ${this.center.lng}`;
+
+  mapOptions: google.maps.MapOptions = {
+    mapTypeId: 'roadmap',
+    zoomControl: true,
+    streetViewControl: false,
+    fullscreenControl: false,
+  };
+
+  updateLocation(event: any) {
+    if (event.latLng != null) {
+      const lat = event?.latLng.lat();
+      const lng = event?.latLng.lng();
+      this.center = { lat, lng };
+      this.markerPosition = { lat, lng };
+      this.locationText = `Lat: ${lat.toFixed(6)}, Lng: ${lng.toFixed(6)}`;
+    }
+  }
+
+  locateMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+
+          this.center = { lat, lng };
+          this.markerPosition = { lat, lng };
+          this.locationText = `Lat: ${lat.toFixed(6)}, Lng: ${lng.toFixed(6)}`;
+          this.zoom = 16;
+        },
+        () => alert('Unable to retrieve location. Check permissions.')
+      );
+    } else {
+      alert('Geolocation not supported');
+    }
+  }
+}
