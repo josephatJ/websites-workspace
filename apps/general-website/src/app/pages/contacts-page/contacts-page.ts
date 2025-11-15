be0b7638464cef042319887b614c8a2d05c94155
Josephat Julius M <josephatjulius24@gmail.com>
2025-11-15 20:18:23 +0300

    Update contacts page

--- apps/general-website/src/app/pages/contacts-page/contacts-page.ts
+++ apps/general-website/src/app/pages/contacts-page/contacts-page.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedPagesWelcomeContainer } from '../../shared/components/shared-pages-welcome-container/shared-pages-welcome-container';
 import { MapsLocationContainer } from '../../shared/components/maps-location-container/maps-location-container';
 import { ContactsUsForm } from '../../shared/components/contacts-us-form/contacts-us-form';
+import { SharedGeneralServiceAndState } from '../../shared/services/general-state.service';
 
 @Component({
   selector: 'app-contacts-page',
@@ -15,4 +16,8 @@ import { ContactsUsForm } from '../../shared/components/contacts-us-form/contact
   templateUrl: './contacts-page.html',
   styleUrl: './contacts-page.css',
 })
-export class ContactsPage {}
+export class ContactsPage {
+  private generalStateService = inject(SharedGeneralServiceAndState);
+  phoneNumbers = this.generalStateService.phoneNumbers;
+  introduction = this.generalStateService.introduction;
+}
