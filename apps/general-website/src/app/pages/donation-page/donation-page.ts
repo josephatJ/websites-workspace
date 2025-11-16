c94728a9d73c6f246c73db3f4e8c1d4b52878f8e
Josephat Julius M <josephatjulius24@gmail.com>
2025-11-16 15:53:15 +0300

    Release: Improve image cover issue

--- apps/general-website/src/app/pages/donation-page/donation-page.ts
+++ apps/general-website/src/app/pages/donation-page/donation-page.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { SharedPagesWelcomeContainer } from '../../shared/components/shared-pages-welcome-container/shared-pages-welcome-container';
 import { Donation } from '../../shared/components/donation/donation';
 import { SharedGeneralServiceAndState } from '../../shared/services/general-state.service';
+import { ScrollTopModule, ScrollTop } from 'primeng/scrolltop';
 
 @Component({
   selector: 'app-donation-page',
-  imports: [SharedPagesWelcomeContainer, Donation],
+  imports: [SharedPagesWelcomeContainer, Donation, ScrollTop],
   templateUrl: './donation-page.html',
   styleUrl: './donation-page.css',
 })
