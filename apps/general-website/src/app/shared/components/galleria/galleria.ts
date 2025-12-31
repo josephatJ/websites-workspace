b37ef552bcc0ac656d82d7d3116a7296e716a4c0
Josephat Julius M <josephatjulius24@gmail.com>
2025-12-31 20:22:18 +0300

    Release: Add support to Order galleria by order number

--- apps/general-website/src/app/shared/components/galleria/galleria.ts
+++ apps/general-website/src/app/shared/components/galleria/galleria.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ImageModule } from 'primeng/image';
 import { GalleriaStateService } from '../../services/galleria.state.service';
 import { CommonModule } from '@angular/common';
+import { orderBy } from 'lodash';
 
 @Component({
   selector: 'app-galleria',
@@ -18,8 +19,9 @@ export class Galleria implements OnInit {
       .loadGalleria(`items/galleria?fields=*,photos.*`)
       .subscribe({
         next: (galleria) => {
-          console.log(galleria);
-          this.galleriaStateService.updateGalleria(galleria);
+          this.galleriaStateService.updateGalleria(
+            orderBy(galleria, ['order'], ['desc'])
+          );
         },
       });
   }
